e9b0b1dc1290bf073606f0d5d01395c68fa8b007
PepeGusXD <168359468+PepeGusXD@users.noreply.github.com>
2024-04-28 10:28:14 -0600

    Create 1-1.py

---
+++ 1-1.py
@@ -0,0 +1,5 @@
+# Sorprende a tus compañeros
+
+import random
+datos = ["Mi afición favorita es jugar videojuegos", "Tengo un gato que se roba el pan", "Creé un juego basado en la saga de Sonic", "No sé cuál es mi comida favorita", "Tengo un canal de YouTube"]
+print(random.choice(datos))
